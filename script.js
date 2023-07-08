6a51d20cf512a63c3fb9d4f919613c7acbddab1c
operatall <muftauabdulafees18@gmail.com>
2023-07-07 23:32:14 -0800

    Add changes to the calculate button

--- script.js
+++ script.js
@@ -1,6 +1,9 @@
  const calculate=() => {
     if (maleInput.value != "" && femaleInput.value != ""){
         display.innerHTML = `<h1 class = "text-success text-center mx-auto"> You are ${Math.round(Math.random()*100)}% compatible</h1>`
+
+        maleInput.value = "";
+        femaleInput.value =""
     }
     else{
         display.innerHTML = `<h3 class = "text-danger"> Pleae fill all details</h3>`
